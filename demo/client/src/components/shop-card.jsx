9962d6560a18b7196ad17a8b42d2798646f4d83b
Sokojoe <SokoJoey@Gmail.com>
2019-09-07 18:27:34 -0400

    Added shop card and Beemo Onesie

---
+++ demo/client/src/components/shop-card.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Card, H4 } from '@blueprintjs/core'
+import './shop-card.css'
+
+export function ShopCard (props) {
+  const styles = {
+    backgroundImage: `url(${props.img})`
+  }
+  console.log(props.name, props.price, props.img)
+  return (
+    <Card className="shop-card-container">
+      <div className="shop-card-img-container" style={styles} />
+      <H4>{props.name}</H4>
+      <p>${props.price}</p>
+    </Card>
+  )
+}
--- demo/client/src/containers/shop.jsx
+++ demo/client/src/containers/shop.jsx
@@ -1,11 +1,44 @@
 import React from 'react'
+import { Overlay, Navbar, Alignment, Icon } from '@blueprintjs/core';
+import { ShopCard } from '../components/shop-card'
+
+const pageStyle = {
+    width: '60rem',
+    margin: '3rem auto 0px auto'
+  };
+
+const iconStyle = {
+    marginRight: '1rem'
+  };
+
+const itemOne = {
+    name: "Beemo Onesie",
+    price: 200.00
+}
 
 export default class Shop extends React.Component {
     render() {
         return (
             <div>
-                Welcome to the Shop
-            </div>
+                <Navbar className='bp3-dark'>
+                <Navbar.Group align={Alignment.CENTER}>
+                <Navbar.Heading>
+                <Icon
+                    style={iconStyle}
+                    iconSize={Icon.SIZE_LARGE}
+                    icon='dollar'
+                ></Icon>
+              Onesies.com
+            </Navbar.Heading>
+          </Navbar.Group>
+        </Navbar>
+        <div style={pageStyle}> 
+            <ShopCard 
+            img={"https://rgmerch-a.akamaihd.net/products/production/1593-60-00/07-15-2019%2017:44:21:426_Beemo_Onesie_Human_2.png"}
+            name={"Beemo Onesie"}
+            price={199.99}/>
+        </div>
+        </div>
         )
     }
 }
