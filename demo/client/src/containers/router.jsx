705ed3c2ab2659bc2153c0689eb26333565b291f
Sokojoe <SokoJoey@Gmail.com>
2019-09-07 18:02:05 -0400

    Added scaffolding for demo shop

--- demo/client/src/containers/router.jsx
+++ demo/client/src/containers/router.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Home } from './home'
 import { Facewall } from './facewall'
+import Shop from './shop'
 import { HashRouter as Router, Route } from 'react-router-dom'
 
 export function AppRouter() {
@@ -8,6 +9,7 @@ export function AppRouter() {
     <Router>
       <Route path="/" exact component={Home} />
       <Route path="/facewall/" component={Facewall} />
+      <Route path="/shop/" component={Shop} />
     </Router>
   )
 }
---
+++ demo/client/src/containers/shop.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class Shop extends React.Component {
+    render() {
+        return (
+            <div>
+                Welcome to the Shop
+            </div>
+        )
+    }
+}
