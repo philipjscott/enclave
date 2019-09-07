222254a510f4544cc757d7ed3c1864ff09dbaf43
Philip Scott <pscott@zeptohost.com>
2019-09-07 11:19:35 -0400

    add enclave endpoint

--- demo/server/controllers/bookface/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const path = require('path')
-const Router = express.Router
-const router = Router()
-
-router.use(express.static(path.join(__dirname, 'src')));
-
-module.exports = router
--- demo/server/server.js
+++ demo/server/server.js
@@ -1,10 +1,9 @@
 const express = require('express')
-const path = require('path')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 8000
 
-app.use('/hello', require('./controllers/hello'))
-app.use('/bookface', require('./controllers/bookface'))
-app.use('/picto', require('./controllers/picto'))
+app.use(bodyParser())
+app.use('/enclave', require('./controllers/enclave'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
