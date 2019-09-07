7d583f1b6803387894fc5e89ab9691045ccaf92f
Philip Scott <pscott@zeptohost.com>
2019-09-07 18:58:36 -0400

    add command validity checker

--- bot/cmd/main.go
+++ bot/cmd/main.go
@@ -4,8 +4,13 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/keybase/go-keybase-chat-bot/kbchat"
+)
+
+const (
+	commandPrefix = '!'
 )
 
 func fail(msg string, args ...interface{}) {
@@ -15,6 +20,12 @@ func fail(msg string, args ...interface{}) {
 
 func alert(msg string, args ...interface{}) {
 	fmt.Fprintf(os.Stderr, msg+"\n", args...)
+}
+
+func sendSelfMessage(kbc *kbchat.API, msg string) (kbchat.SendResponse, error) {
+	username := kbc.GetUsername()
+	tlfName := fmt.Sprintf("%s,%s", username, username)
+	return kbc.SendMessageByTlfName(tlfName, msg)
 }
 
 func main() {
@@ -29,24 +40,43 @@ func main() {
 		fail("Error creating API: %s", err.Error())
 	}
 
-	username := kbc.GetUsername()
-	tlfName := fmt.Sprintf("%s,%s", username, username)
-	if _, err = kbc.SendMessageByTlfName(tlfName, "EnclaveBot listening!"); err != nil {
+	if _, err = sendSelfMessage(kbc, "EnclaveBot listening!"); err != nil {
 		fail("Error sending message; %s", err.Error())
 	}
 	if sub, err := kbc.ListenForNewTextMessages(); err != nil {
 		fail("Error creating subscription; %s", err.Error())
 	} else {
-		listen(sub)
+		listen(kbc, sub)
 	}
 }
 
-func listen(sub kbchat.NewSubscription) {
+func listen(kbc *kbchat.API, sub kbchat.NewSubscription) {
 	for {
 		msg, err := sub.Read()
 		if err != nil {
 			alert("Error receiving chat message; %s", err.Error())
 		}
-		fmt.Println(msg.Message.Content.Text.Body)
+		if msg.Message.Content.Text == nil {
+			continue
+		}
+		body := msg.Message.Content.Text.Body
+		fragments := strings.Split(body, " ")
+		if len(fragments) == 0 {
+			continue
+		}
+		command := fragments[0]
+		if command[0] != commandPrefix {
+			continue
+		}
+		command = command[1:]
+		switch command {
+		case "group":
+		case "data":
+		default:
+			_, err := sendSelfMessage(kbc, fmt.Sprintf("Invalid command: %s\n", command))
+			if err != nil {
+				alert("Error receiving chat message; %s", err.Error())
+			}
+		}
 	}
 }
