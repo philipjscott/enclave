fdcd03456588bcad782e9e00f003fc7f349a87ec
Philip Scott <pscott@zeptohost.com>
2019-09-07 01:06:39 -0400

    add static files

--- demo/server.js
+++ demo/server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 const port = 3000
 
@@ -6,5 +7,6 @@ app.use('/hello', require('./apps/hello'))
 
 app.use('/bookface', require('./apps/bookface'))
 app.use('/picto', require('./apps/picto'))
+app.use('/static', express.static(path.join(__dirname, './dist')))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
---
+++ demo/static/index.js
@@ -0,0 +1,1 @@
+console.log('js bundling works lol')
