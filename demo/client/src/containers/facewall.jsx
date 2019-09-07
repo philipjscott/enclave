eb31008759b6254b016b80e062ada13c352bbda9
Sokojoe <SokoJoey@Gmail.com>
2019-09-07 12:54:21 -0400

    Modal work started

--- demo/client/src/containers/facewall.jsx
+++ demo/client/src/containers/facewall.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Face } from '../components/face'
+import {AddFace} from '../components/add-face'
 
 const imgURL = 'https://avatars3.githubusercontent.com/u/18666879?s=400&u=612a253a6e160f917b083d7e742d1a0a7e0c19cb&v=4'
 const name = 'Philip Scott'
@@ -9,6 +10,7 @@ export function Facewall () {
     <div>
       <h1>Facewall</h1>
       <Face src={imgURL} name={name} />
+      <AddFace/>
     </div>
   )
 }
