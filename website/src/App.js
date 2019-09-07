79cddedb0d3da4dfb4c6397ddd05968ed8736169
Sokojoe <SokoJoey@Gmail.com>
2019-09-07 12:31:58 -0400

    Added initial enclave website

--- website/src/App.js
+++ website/src/App.js
@@ -1,23 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import backgroundImage from'./image.jpg'
+
+const inlineStyle = {
+  backgroundImage: `url(${backgroundImage})`,
+  backgroundPosition: 'center',
+  backgroundSize: 'cover',
+  backgroundRepeat: 'no-repeat'
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <header className="App-header" style={inlineStyle}>
+        <div className="header-title"> 
+        Enclave
+        </div> 
+        <p className="header-subtitle">
+          Privacy by design
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
