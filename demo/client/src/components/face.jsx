b66aea0c7fe3bb8079e4253d7dc8b1107d327d78
Philip Scott <pscott@zeptohost.com>
2019-09-07 12:29:27 -0400

    add face card

---
+++ demo/client/src/components/face.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Card, H4 } from '@blueprintjs/core'
+import './face.css'
+
+export function Face (props) {
+  const styles = {
+    backgroundImage: `url(${props.src})`
+  }
+  return (
+    <Card className="face-container">
+      <div className="face-img-container" style={styles} />
+      <H4>{props.name}</H4>
+    </Card>
+  )
+}
