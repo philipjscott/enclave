ea5522afd0906e11fd3d3dbb009a0ac03d481e5d
Wesley <wesleykhma@gmail.com>
2019-09-07 18:56:52 -0400

    added homepage links

--- demo/client/src/containers/shop.jsx
+++ demo/client/src/containers/shop.jsx
@@ -1,44 +1,47 @@
-import React from 'react'
+import React from 'react';
 import { Overlay, Navbar, Alignment, Icon } from '@blueprintjs/core';
-import { ShopCard } from '../components/shop-card'
+import { ShopCard } from '../components/shop-card';
 
 const pageStyle = {
-    width: '60rem',
-    margin: '3rem auto 0px auto'
-  };
+  width: '60rem',
+  margin: '3rem auto 0px auto'
+};
 
 const iconStyle = {
-    marginRight: '1rem'
-  };
+  marginRight: '1rem'
+};
 
 const itemOne = {
-    name: "Beemo Onesie",
-    price: 200.00
-}
+  name: 'Beemo Onesie',
+  price: 200.0
+};
 
 export default class Shop extends React.Component {
-    render() {
-        return (
-            <div>
-                <Navbar className='bp3-dark'>
-                <Navbar.Group align={Alignment.CENTER}>
-                <Navbar.Heading>
-                <Icon
-                    style={iconStyle}
-                    iconSize={Icon.SIZE_LARGE}
-                    icon='dollar'
-                ></Icon>
+  render() {
+    return (
+      <div>
+        <Navbar className='bp3-dark'>
+          <Navbar.Group align={Alignment.CENTER}>
+            <Navbar.Heading>
+              <Icon
+                style={iconStyle}
+                iconSize={Icon.SIZE_LARGE}
+                icon='shop'
+              ></Icon>
               Onesies.com
             </Navbar.Heading>
           </Navbar.Group>
         </Navbar>
-        <div style={pageStyle}> 
-            <ShopCard 
-            img={"https://rgmerch-a.akamaihd.net/products/production/1593-60-00/07-15-2019%2017:44:21:426_Beemo_Onesie_Human_2.png"}
-            name={"Beemo Onesie"}
-            price={199.99}/>
+        <div style={pageStyle}>
+          <ShopCard
+            img={
+              'https://rgmerch-a.akamaihd.net/products/production/1593-60-00/07-15-2019%2017:44:21:426_Beemo_Onesie_Human_2.png'
+            }
+            name={'Beemo Onesie'}
+            price={199.99}
+          />
         </div>
-        </div>
-        )
-    }
+      </div>
+    );
+  }
 }
