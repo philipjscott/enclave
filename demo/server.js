c491f8a2effb3f094addffd4430c4bc5bc9cc104
Philip Scott <pscott@zeptohost.com>
2019-09-07 00:43:56 -0400

    add nodemon and picto

--- demo/server.js
+++ demo/server.js
@@ -5,5 +5,6 @@ const port = 3000
 app.use('/hello', require('./apps/hello'))
 
 app.use('/bookface', require('./apps/bookface'))
+app.use('/picto', require('./apps/picto'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
