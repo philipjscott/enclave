33ac5031d1a73533d1fc6d826943355b216d76ff
Philip Scott <pscott@zeptohost.com>
2019-09-07 21:40:14 -0400

    hit server endpoint

--- demo/client/src/api/rest.js
+++ demo/client/src/api/rest.js
@@ -3,7 +3,12 @@ import axios from 'axios'
 const serverURL = process.env.REACT_APP_SERVER_URL
 const getURL = endpoint => `${serverURL}${endpoint}`
 
-export function getProfile(profile) {
+export function getPublicProfile(profile) {
   const url = getURL('/pub')
   return axios.post(url, { profile }).then(res => res.data)
+}
+
+export function getPrivateProfile(profile, password) {
+  const url = getURL('/shop')
+  return axios.post(url, { profile, password }).then(res => res.data)
 }
