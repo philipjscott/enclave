f9bb2d6bc4323cc0a46ccb373f6ae33e5ee6770d
Philip Scott <pscott@zeptohost.com>
2019-09-07 21:34:33 -0400

    add endpoint for shop

--- demo/server/server.js
+++ demo/server/server.js
@@ -7,5 +7,6 @@ const port = 8000
 app.use(cors())
 app.use(bodyParser())
 app.use('/pub', require('./controllers/pub'))
+app.use('/shop', require('./controllers/shop'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
